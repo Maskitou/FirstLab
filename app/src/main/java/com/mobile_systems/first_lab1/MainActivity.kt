dbe780842759a6a3bb1375b9eef077e52ef54f09
Maxim <shimkusmaxim@gmail.com>
2024-10-04 14:14:06 +0700

    Добавил объекты на конструктор

--- app/src/main/java/com/mobile_systems/first_lab1/MainActivity.kt
+++ app/src/main/java/com/mobile_systems/first_lab1/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
